f3c51f280549d12cb6bcf1a5b84a2756c1c0f2f7
Ahmed Soliman <ahmedanwar@mail.com>
2016-11-06 10:29:50 -0800

    finished coding the carousel

--- js/main.js
+++ js/main.js
@@ -12,4 +12,32 @@ $(document).ready(function() {
 	$(".expect-text").css("display", "none")
 	firstMonthContent.css("display", "block")
 
+	$("#monthOne").click(function(event) {
+		$(".expect-text").css("display", "none")
+		firstMonthContent.fadeIn(1000)
+
+		$("#monthTwo, #monthThree").removeClass("selected")
+		$(this).addClass("selected")
+		event.preventDefault();
+	})
+
+	$("#monthTwo").click(function(event) {
+		$(".expect-text").css("display", "none")
+			secondMonthContent.fadeIn(1000)
+
+			$("#monthOne, #monthThree").removeClass("selected")
+			$(this).addClass("selected")
+
+		event.preventDefault();
+	})
+
+	$("#monthThree").click(function(event) {
+		$(".expect-text").css("display", "none")
+			thirdMonthContent.fadeIn(1000)
+
+			$("#monthOne, #monthTwo").removeClass("selected")
+			$(this).addClass("selected")
+
+		event.preventDefault();
+	})
 })
