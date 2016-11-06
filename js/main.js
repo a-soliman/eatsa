d9498d8262bdd211daf9521867ee5f64429a6f6e
Ahmed Soliman <ahmedanwar@mail.com>
2016-11-06 10:28:11 -0800

    coding the js for the carousel in my Plan page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
 	//Nav Bar Slide Down
 	$(".navbar").slideDown(2000)
 
+	//My Plan Car
+	var firstMonthContent = $(".firstMonth")
+	var secondMonthContent = $(".secondMonth")
+	var thirdMonthContent = $(".thirdMonth")
+
 })
