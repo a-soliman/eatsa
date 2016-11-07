c42f1f116183bcafaafcb5ab15eb432da3a1bf41
Ahmed Soliman <ahmedanwar@mail.com>
2016-11-07 10:44:55 -0800

    coded the route provider roles for the navigation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 var app = angular.module('multiPageApp', ['ngRoute'])
 
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/home.html'
+	})
+	.when('/whyMe', {
+		templateUrl: 'views/whyme.html'
+	})
+	.when('/plan', {
+		templateUrl: 'views/plan.html'
+	})
+	.when('/story', {
+		templateUrl: 'views/story.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
 })
